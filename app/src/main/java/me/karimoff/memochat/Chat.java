cb991eff71d92f14ac9866350d7a1f8178dfca31
Mukhammad Karimov <hey@karimoff.me>
2017-08-06 14:46:51 +0900

    chat activity

---
+++ app/src/main/java/me/karimoff/memochat/Chat.java
@@ -0,0 +1,34 @@
+package me.karimoff.memochat;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by karimoff on 8/6/17.
+ */
+
+public class Chat {
+    private Date created;
+    private List<Message> messages;
+
+    public Chat(Date created, List<Message> messages) {
+        this.created = created;
+        this.messages = messages;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    public List<Message> getMessages() {
+        return messages;
+    }
+
+    public void setMessages(List<Message> messages) {
+        this.messages = messages;
+    }
+}
--- app/src/main/java/me/karimoff/memochat/ChatListFragment.java
+++ app/src/main/java/me/karimoff/memochat/ChatListFragment.java
@@ -1,17 +1,21 @@
 package me.karimoff.memochat;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -20,9 +24,11 @@ import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnItemClick;
 
 
 /**
@@ -37,6 +43,8 @@ public class ChatListFragment extends Fragment {
     ArrayList<String> friendsList = new ArrayList<>();
     ArrayAdapter<String> friendsListAdapter;
 
+    private FirebaseAuth mAuth;
+    FirebaseUser user;
     private DatabaseReference mDatabase;
 
     public ChatListFragment() {
@@ -47,6 +55,8 @@ public class ChatListFragment extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mDatabase = FirebaseDatabase.getInstance().getReference();
+        mAuth = FirebaseAuth.getInstance();
+        user = mAuth.getCurrentUser();
     }
 
     @Override
@@ -87,5 +97,37 @@ public class ChatListFragment extends Fragment {
 
         friendsListView.setAdapter(friendsListAdapter);
 
+    }
+
+    @OnItemClick(R.id.friendsListView)
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        final String room1 = user.getUid() + "_" + friendsList.get(i);
+        final String room2 = friendsList.get(i) + "_" + user.getUid();
+        final DatabaseReference chats = mDatabase.child("chats");
+
+        chats.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if(dataSnapshot.hasChild(room1)){
+                    startChatActivity(room1);
+                } else if(dataSnapshot.hasChild(room2)){
+                    startChatActivity(room2);
+                } else {
+                    chats.child(room1).setValue(new Chat(new Date(), null));
+                    startChatActivity(room1);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+    public void startChatActivity(String room){
+        Intent intent = new Intent(getActivity(), ChatActivity.class);
+        intent.putExtra("room", room);
+        startActivity(intent);
     }
 }
